1f548e817f185c4c5ba6637c06d1dcfb134e2145
Wojciech Gunia <wojciech.gunia@outlook.com>
2022-09-13 17:00:52 +0200

    Kurs Angular - początek

--- Angular/Angular-kurs/Lista-zadan/README.md
+++ Angular/Angular-kurs/Lista-zadan/README.md
@@ -1,4 +1,4 @@
-# AngularKurs
+# ListaZadan
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.1.
 
--- Angular/Angular-kurs/Lista-zadan/karma.conf.js
+++ Angular/Angular-kurs/Lista-zadan/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-kurs'),
+      dir: require('path').join(__dirname, './coverage/lista-zadan'),
       subdir: '.',
       reporters: [
         { type: 'html' },
